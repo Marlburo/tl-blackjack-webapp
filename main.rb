352fae32bab8eb02917654e085b8749870203c33
Marlburo <lui_ca45@hotmail.com>
2014-08-10 18:28:41 -0400

    Signed-off-by: Marlburo

--- main.rb
+++ main.rb
@@ -1,7 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 
+
 set :sessions, true
+
+get '/' do
+	if session[:player_name]
+		redirect '/game'
+	else
+		redirect '/new_player'
+	end
+end
+
+get '/new_player' do
+	erb :new_player
+end
 
 
 
