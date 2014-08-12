57ed899441c4601fafe5aef42bdb171137f98eb1
Marlburo <lui_ca45@hotmail.com>
2014-08-11 21:34:57 -0400

    Signed-off-by: Marlburo

--- main.rb
+++ main.rb
@@ -4,6 +4,41 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+
+	def check_cards(card_review)
+  	if (card_review == 'J' || card_review == 'Q' || card_review == 'K')
+    	return 10
+  	elsif card_review == 'A'
+  		session[:aces_available] += 1 
+    	return 11
+  	else 
+    	return card_review.to_i
+  	end
+	end
+
+	def card_image(card)
+		suit = case card[0]
+			when 'H' then 'hearts'
+			when 'D' then 'diamonds'
+			when 'C' then 'clubs'
+			when 'S' then 'spades'
+		end
+
+		value = card[1]
+		if['J', 'Q', 'K', 'A'].include?(value)
+			value = case card[1]
+				when 'J' then 'jack'
+				when 'Q' then 'queen'
+				when 'K' then 'king'
+				when 'A' then 'ace'
+			end
+		end
+		"<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+	end
+end
+
+
 get '/' do
 	if session[:player_name]
 		redirect '/game'
@@ -16,6 +51,25 @@ get '/new_player' do
 	erb :new_player
 end
 
+post '/new_player' do
+  	session[:player_name] = params[:player_name]
+  	redirect '/game'
+end
 
+get '/game' do
+  # create a deck and put it in session
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle! # [ ['H', '9'], ['C', 'K'] ... ]
 
+  # deal cards
+  session[:aces_available] = 0
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
 
+  erb :game
+end
